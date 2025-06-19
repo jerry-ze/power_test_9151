770daf567e5410d9cb4fe6fe0c2f22684c666c1e
zengzhi <zengzhi@we-wins.com>
2025-06-19 09:41:40 +0800

    9151硬件最低功耗测试

--- src/main.c
+++ src/main.c
@@ -10,6 +10,10 @@
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/sys/printk.h>
+#include <zephyr/pm/device.h>
+#include <modem/nrf_modem_lib.h>
+
+const struct device *uart = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
 
 #define GPIO0_NODE DT_NODELABEL(gpio0)
 
@@ -24,28 +28,31 @@
 #define BUZZER			26
 
 
-
 int main(void)
 {
 	const struct device *gpio0_dev = DEVICE_DT_GET(GPIO0_NODE);
 	if (!device_is_ready(gpio0_dev)) {
 		printk("device gpio0_dev error!\n");
     }
+	int err = nrf_modem_lib_init();
+    if (err)
+    {
+        printk("Failed to initialize modem library, error: %d\n", err);
+    }
 
 	gpio_pin_configure(gpio0_dev, nRF70_VBAT_EN, GPIO_OUTPUT_LOW);
 	gpio_pin_configure(gpio0_dev, WIFI_VDDIO_EN, GPIO_OUTPUT_LOW);
 	gpio_pin_configure(gpio0_dev, WIFI_EN, GPIO_OUTPUT_LOW);
-
 	gpio_pin_configure(gpio0_dev, LED_RED, GPIO_OUTPUT_LOW);
 	gpio_pin_configure(gpio0_dev, LED_BLUE, GPIO_OUTPUT_LOW);
 	gpio_pin_configure(gpio0_dev, LED_GREEN, GPIO_OUTPUT_LOW);
-
 	gpio_pin_configure(gpio0_dev, BUZZER, GPIO_OUTPUT_LOW);
 
+	pm_device_action_run(uart, PM_DEVICE_ACTION_SUSPEND);
+
 	for(;;){
-//		printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
-		k_msleep(1000);
-		
+		printf("Hello World! %s\n", CONFIG_BOARD_TARGET);
+		k_msleep(2000);
 	}
 	return 0;
 }
